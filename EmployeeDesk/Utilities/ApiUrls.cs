2d6b7e016a60f577727d6eddea459463533ae5c7
Murali <vmkrishna111@gmail.com>
2023-04-05 20:22:32 +0530

    changes

---
+++ EmployeeDesk/Utilities/ApiUrls.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDesk.Utilities
+{
+    public class ApiUrls
+    {
+        public const string baseURI = "https://gorest.co.in/public-api/";
+        public const string emplist = "users";
+    }
+}
